7baca554585d1bff62d570688a57c22664f841ba
merryroger <merry_roger@yahoo.com>
2019-01-16 22:37:41 +0500

    Other corrections have been made

--- app/User.php
+++ app/User.php
@@ -5,7 +5,6 @@ namespace App;
 use App\Post;
 
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
